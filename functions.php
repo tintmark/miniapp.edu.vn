7ddae47434776c342f2e9ba06ceba1b45e94bda6
tintmark <thanhtindalat@gmail.com>
2026-04-23 00:27:39 +0700

    Cập nhật email liên hệ và cấu trúc theme

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
-function mi9vn_enqueue_styles() {
-    wp_enqueue_style( 'mi9vn-style', get_stylesheet_uri() );
+function miniai_workshop_enqueue_styles() {
+    wp_enqueue_style( 'miniai-workshop-style', get_stylesheet_uri() );
 }
-add_action( 'wp_enqueue_scripts', 'mi9vn_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'miniai_workshop_enqueue_styles' );
 
-function mi9vn_setup() {
+function miniai_workshop_setup() {
     add_theme_support( 'title-tag' );
 }
-add_action( 'after_setup_theme', 'mi9vn_setup' );
+add_action( 'after_setup_theme', 'miniai_workshop_setup' );
